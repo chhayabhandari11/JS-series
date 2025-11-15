95a0b30b0d8233d374790bfd316ac172d5be99a1
chhayabhandari11 <149098953+chhayabhandari11@users.noreply.github.com>
2025-11-15 06:39:16 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accId= 123456645
+let accEmail= "chhayab32@gmail.com"
+var accPass="12345"
+accCity="delhi"
+console.log(accId);
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,3 @@
+"use strict"; 
+//alert(3+3) // we are using nodejs not browser
+console.log(3+3); console.log("cjshc");
